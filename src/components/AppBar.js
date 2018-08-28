03d3ec19289bd2418149c1fcf991f5232aaedffd
Debotos Das <debotosdas@gmail.com>
2018-08-28 12:19:47 +0600

    added routine pdf download link

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Toolbar } from 'react-native-material-ui';
 import store from 'react-native-simple-store';
+import { Linking } from 'react-native';
+
+const ROUTINE_PDF_API = 'https://bitc-routine-dev.herokuapp.com/api/client/pdf';
 export default class AppBar extends React.Component {
   onChangeText = text => {
     this.setState({ search: text });
@@ -13,6 +16,20 @@ export default class AppBar extends React.Component {
     return (
       <Toolbar
         //leftElement="menu"
+        rightElement={{
+          menu: {
+            icon: 'more-vert',
+            labels: ['Download PDF']
+          }
+        }}
+        onRightElementPress={label => {
+          console.log('Label Pressed => ', label);
+          if (label.index === 0) {
+            Linking.openURL(ROUTINE_PDF_API).catch(err =>
+              console.error('An error occurred', err)
+            );
+          }
+        }}
         centerElement="BITC Class Routine"
         searchable={{
           autoFocus: true,
